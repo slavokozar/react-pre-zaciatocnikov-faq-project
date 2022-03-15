b8a1ad6a6cc2d028b483d8f28452303ebf69e795
Slavo Kozar <slavo.kozar@gmail.com>
2022-03-15 15:35:46 +0100

    stav pred lekciou 2

---
+++ src/FaqItem.js
@@ -0,0 +1,11 @@
+function FaqItem(){
+    return (
+        <div>
+            <strong>Je akademie zdarma?</strong>
+            <p>Akademie je zdarma pro všechny členy klubu Silicon Hill s platním základním a síťovím členstvím.</p>
+            <button>Ukaž odpověď</button>
+        </div>
+    )
+}
+
+export default FaqItem;
