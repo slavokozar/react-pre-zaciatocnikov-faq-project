45995096d33f95452b651a591adb0deeab5b8162
Slavo Kozar <slavo.kozar@gmail.com>
2023-04-06 19:26:25 +0200

    zima2022

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
 
   return (
     <div className="App">
-      <FaqItem/>
+      <FaqItem
+          question="Je akademie zdarma?"
+          answer="Akademie je zdarma pro všechny členy klubu Silicon Hill s platním základním a síťovím členstvím."
+      />
+      <FaqItem
+          question="Co si vzít sebou?"
+          answer="Vlastní notebook."
+      />
+      <FaqItem
+          question="Co musím mít nainstalováno v notebooku?"
+          answer="Node.JS, Editor zdrojového kódu, např. Visual Studio Code."
+      />
     </div>
   );
 }
--- src/FaqItem.js
+++ src/FaqItem.js
@@ -1,27 +1,27 @@
 import {useState} from 'react';
 
 
-function FaqItem(){
+function FaqItem(props) {
     const [open, setOpen] = useState(false);
 
-    console.log(open);
+    console.log(props);
 
-    function hello(){ 
-        // alert('Hello')
+    function hello() {
         setOpen(!open);
     }
 
     return (
         <div>
-            <strong>Je akademie zdarma?</strong>
+            <strong>{props.question}</strong>
             {
                 open && (
-                    <p>Akademie je zdarma pro všechny členy klubu Silicon Hill s platním základním a síťovím členstvím.</p> 
+                    <p>{props.answer}</p>
                 )
             }
 
-            <button onClick={hello} >
-                { open ? 'Schovej odpověď' : 'Ukaž odpověď' }
+
+            <button onClick={hello}>
+                {open ? 'Schovej odpověď' : 'Ukaž odpověď'}
             </button>
         </div>
     )
